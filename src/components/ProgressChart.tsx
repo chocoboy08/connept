9fafa9f0786d2100443d776a35e27275f66872a7
chocoboy08 <chocoboy0508@gmail.com>
2025-10-31 03:50:43 +0900

    feat: 진행도 도넛 차트 구현

---
+++ src/components/ProgressChart.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import Text from "./base/Text";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+interface ProgressChartProps {
+  percent: number;
+}
+
+export default function ProgressChart({ percent }: ProgressChartProps) {
+  const data = {
+    datasets: [
+      {
+        data: [100 - percent, percent],
+        backgroundColor: ["transparent", "#EF4444"],
+        borderRadius: 20,
+        borderWidth: 0,
+        cutout: "70%",
+      },
+    ],
+  };
+  const back = {
+    datasets: [
+      {
+        data: [100],
+        backgroundColor: ["#E5E7EB"],
+        borderWidth: 0,
+        cutout: "70%",
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      legend: { display: false },
+      tooltip: { enabled: false },
+    },
+  };
+  return (
+    <div style={{ position: "relative", width: "fit-content" }}>
+      <div style={{ width: 114 }}>
+        <Doughnut data={back} options={options} />
+      </div>
+      <div style={{ width: 114, position: "absolute", top: 0, zIndex: 1 }}>
+        <Doughnut data={data} options={options} />
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Text size={25} weight={600} color="#5C5C5C">
+          {percent}
+        </Text>
+      </div>
+    </div>
+  );
+}
