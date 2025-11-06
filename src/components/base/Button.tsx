63f9f2af259e6940c2399ae56f713469e1d0fc57
chocoboy08 <chocoboy0508@gmail.com>
2025-11-06 16:49:51 +0900

    feat: Button 컴포넌트에 outline 버튼 속성 추가, 텍스트 조절 기능 추가

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,27 +1,48 @@
 import { CSSProperties, PropsWithChildren } from "react";
+import Text from "./Text";
 
 interface ButtonProps extends PropsWithChildren {
-  bgColor?: string;
+  type?: "normal" | "outline";
   width?: CSSProperties["width"];
   height?: CSSProperties["height"];
+  fontSize?: CSSProperties["fontSize"];
+  onClickHandler: () => void;
+  color?: CSSProperties["color"];
+  fontWeight?: 400 | 600 | 700;
 }
 
 export default function Button({
   children,
-  bgColor = "#725AF5",
+  type = "normal",
   width = "100%",
   height = "fit-content",
+  fontSize,
+  onClickHandler,
+  color,
+  fontWeight = 600,
 }: ButtonProps) {
   return (
     <button
-      className="flex justify-center items-center rounded-lg cursor-pointer"
+      onClick={onClickHandler}
+      className={`flex justify-center items-center rounded-xl cursor-pointer ${
+        type === "normal" ? "bg-main" : `bg-white border border-main`
+      }`}
       style={{
-        backgroundColor: bgColor,
         width: width,
         height: height,
+        ...(color &&
+          type === "outline" && { borderColor: color, color: color }),
       }}
     >
-      {children}
+      <Text
+        size={fontSize}
+        weight={fontWeight}
+        color={
+          color ? color : type === "normal" ? "white" : "var(--color-main)"
+        }
+      >
+        {children}
+      </Text>
     </button>
   );
 }
