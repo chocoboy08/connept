c4d3c89194da174264cfde743e5002e557e8eaa1
chocoboy08 <chocoboy0508@gmail.com>
2025-11-13 17:53:48 +0900

    feat: 학습기록 로드맵 추가

---
+++ src/components/StudyRoadMap.tsx
@@ -0,0 +1,40 @@
+import Box from "./base/Box";
+import Flex from "./base/Flex";
+import Text from "./base/Text";
+import StudyRoadMapItem from "./StudyRoadMapItem";
+
+const mock = [
+  { title: "미분계수", progress: 100 },
+  { title: "삼각함수", progress: 50 },
+  { title: "합성함수의 미분법", progress: 0 },
+];
+export default function StudyRoadMap() {
+  return (
+    <Box paddingX={28} paddingY={25} center={false}>
+      <Flex direction="column" gap={34}>
+        <Text as={"h1"} size={22} weight={700}>
+          학습 로드맵
+        </Text>
+        <Flex direction="column" className="w-75">
+          {mock.map((item, idx) => (
+            <Flex gap={12} key={`roadmap-${item}-${idx}`}>
+              <Flex direction="column" align="center">
+                <div
+                  className={`size-2.5 rounded-full ${
+                    item.progress === 100
+                      ? "bg-correct"
+                      : item.progress === 0
+                      ? "bg-[#D1D5DB]"
+                      : "bg-main"
+                  }`}
+                />
+                <hr className="w-0.5 bg-[#D1D5DB] border-none flex-1" />
+              </Flex>
+              <StudyRoadMapItem title={item.title} isChain />
+            </Flex>
+          ))}
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
---
+++ src/components/StudyRoadMapItem.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { IconArrowDown } from "@/assets/icons";
+import { useState } from "react";
+import Flex from "./base/Flex";
+import Text from "./base/Text";
+
+interface StudyRoadMapItemProp {
+  title: string;
+  isChain: boolean;
+}
+
+export default function StudyRoadMapItem({ title }: StudyRoadMapItemProp) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Flex direction="column" className="flex-1 -mt-10 py-6">
+      <button
+        className="flex items-center justify-between py-2"
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+      >
+        <Flex gap={6} align="center">
+          <Text>{title}</Text>
+          <div className={`rounded-full px-2.5 py-1.5 bg-[#E6DFFF]`}>
+            <Text size={12} color={"var(--color-main)"}>
+              연계
+            </Text>
+          </div>
+        </Flex>
+        <Flex
+          className="size-6 rounded-full bg-[#F3F4F6]"
+          align="center"
+          justify="center"
+        >
+          <IconArrowDown width={16} height={16} />
+        </Flex>
+      </button>
+
+      {isOpen && (
+        <Flex gap={5} align="center">
+          <div className="w-1.5 h-1.5 rounded-full bg-[#6B7280]" />
+
+          <Text size={12} color="#6B7280">
+            지수함수 미분
+          </Text>
+        </Flex>
+      )}
+    </Flex>
+  );
+}
